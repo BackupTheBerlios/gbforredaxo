e5ec5596282c748dc5d4333788f4a492536541a9
koala_s <koala_s>
2006-06-24 22:48:09 +0000

    Funktion zum automatischen Verbinden der Action mit dem Modul während der Installation hinzugefügt

--- guestbook/install.inc.php
+++ guestbook/install.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: install.inc.php,v 1.1 2006/06/13 20:26:08 koala_s Exp $
+ * @version $Id: install.inc.php,v 1.2 2006/06/24 22:48:09 koala_s Exp $
  */
  
 $error = '';
@@ -17,9 +17,14 @@ if (!OOAddon :: isAvailable('addon_framework'))
 if ($error == '')
 {
   $error .= rex_installAddon(dirname(__FILE__).'/install.sql');
+  include(dirname(__FILE__).'/install.php');
+  // installAction2Modul(Name des Modules, Name der Action)
+  $error .= installAction2Modul('Gästebuch - Eintragslis', 'Gästebuch - Eintragsliste StatusPerDatei');
 }
 
-if ($error != '')
+// Funktionen dürfen ruhig auch mal ein TRUE zurückgeben
+// deshalb prüfen der error-Variablen auf !== TRUE
+if ($error != '' and $error !== TRUE)
 {
   $REX['ADDON']['installmsg']['guestbook'] = $error;
 }
--- guestbook/install.php
+++ guestbook/install.php
@@ -7,7 +7,7 @@
  *  
  * @author Koala
  * @package redaxo3
- * @version $Id: install.php,v 1.1 2006/06/24 22:03:18 koala_s Exp $
+ * @version $Id: install.php,v 1.2 2006/06/24 22:48:23 koala_s Exp $
  */
 
 /**
@@ -15,7 +15,7 @@
  * Die install.sql muss bereits ausgeführt worden sein und die Module und 
  * Action somit bereits in der Datenbank stehen. 
  * Als erstes werden die IDs des Modules "Gästebuch - Eintragsliste" und der 
- * Action "Gästebuch - Eintragsliste StstusPerdatei" ausgelesen und dann nachgeshen,
+ * Action "Gästebuch - Eintragsliste StstusPerdatei" ausgelesen und dann nachgesehen,
  * ob es dazu schon eine Zuweisung in der Tabelle rex_module_action gibt.
  * Ist das nicht der Fall, werden die IDs entsprechend eingetragen.
  * 
@@ -23,51 +23,65 @@
  * Im Fehlerfalle muss eine Meldung ausgegeben werden.
  * Die Action könnte dann evtl. per Hand noch zugewiesen werden.   
  * 
- */
-
-
-/**
- * Diese Abfrage gibt zurück
- * - wenn es bereits eine Verküpfung in der Tabelle rex_module_action gibt:
- * m_id  a_id  mod_action_m_id   mod_action_a_id 
- *  42     9       true             true
  * 
- * - gibt es noch keine Verknüpfung, sieht die Rückgabe so aus:
- * m_id  a_id  mod_action_m_id   mod_action_a_id 
- *  42     9       false             false
- *
- * m_id und a_id sind von MySQL vergebene IDs und entsprechen nicht diesem Beispiel hier!
- * 
+ * @param   string  Name des Modules (auf richtige Schreibweise achten!)
+ * @param   string  Name der Action (auf richtige Schreibweise achten!)
+ * @return  mixed   TRUE oder ein Fehlertext
  */
-$qry = 'SELECT `'.$REX['TABLE_PREFIX'].'modultyp`.`id` AS m_id, `'.$REX['TABLE_PREFIX'].'action`.`id` AS a_id,
-          IF(`'.$REX['TABLE_PREFIX'].'module_action`.`module_id` != 0, "true", "false") AS mod_action_m_id,
-          IF(`'.$REX['TABLE_PREFIX'].'module_action`.`action_id` != 0, "true", "false") AS mod_action_a_id
-        FROM `'.$REX['TABLE_PREFIX'].'modultyp` , `'.$REX['TABLE_PREFIX'].'action` 
-        LEFT JOIN `'.$REX['TABLE_PREFIX'].'module_action` ON ( `'.$REX['TABLE_PREFIX'].'module_action`.`module_id` = `'.$REX['TABLE_PREFIX'].'modultyp`.`id` 
-          AND `'.$REX['TABLE_PREFIX'].'module_action`.`action_id` = `'.$REX['TABLE_PREFIX'].'action`.`id` ) 
-        WHERE `'.$REX['TABLE_PREFIX'].'modultyp`.`name` = "Gästebuch - Eintragslist"
-          AND `'.$REX['TABLE_PREFIX'].'action`.`name` = "Gästebuch - Eintragsliste StatusPerDatei"
-        LIMIT 1';
+function installAction2Modul($modul_name, $action_name) {
+  global $REX;
 
-$sql = new sql();
-$sql->debugsql = true;
-$data = $sql->get_array($qry);
-
-
-if (is_array($data)) { $i=1;
-  foreach ($data as $row) {
-    // prüfe IDs auf vorhandensein
-    // sind diese IDs in dieser Kombination noch nicht in der Verknüpfungstabelle
-    // dann können sie dort eingetragen werden
-    if ($row['mod_action_m_id'] == 'false' and $row['mod_action_a_id'] == 'false') {
-      $qry = 'INSERT INTO `'.$REX['TABLE_PREFIX'].'module_action` ( `id` , `module_id` , `action_id` ) 
-              VALUES (NULL , "'.$row['m_id'].'", "'.$row['a_id'].'")';
-      $sql = new sql();
-      $sql->setQuery($qry);
-    }
+  if (!isset ($modul_name) or $modul_name == '' or !isset ($action_name) or $action_name == '') {
+    return 'installAction2Modul: Keinen Modul- oder Aktionname übergeben.';
   }
-}
 
+  /**
+   * Diese Abfrage gibt zurück
+   * - wenn es bereits eine Verküpfung in der Tabelle rex_module_action gibt:
+   * m_id  a_id  mod_action_m_id   mod_action_a_id 
+   *  42     9       true             true
+   * 
+   * - gibt es noch keine Verknüpfung, sieht die Rückgabe so aus:
+   * m_id  a_id  mod_action_m_id   mod_action_a_id 
+   *  42     9       false             false
+   *
+   * m_id und a_id sind von MySQL vergebene IDs und entsprechen nicht diesem Beispiel hier!
+   * 
+   */
+  $qry = 'SELECT `'.$REX['TABLE_PREFIX'].'modultyp`.`id` AS m_id, `'.$REX['TABLE_PREFIX'].'action`.`id` AS a_id,
+            IF(`'.$REX['TABLE_PREFIX'].'module_action`.`module_id` != 0, "true", "false") AS mod_action_m_id,
+            IF(`'.$REX['TABLE_PREFIX'].'module_action`.`action_id` != 0, "true", "false") AS mod_action_a_id
+          FROM `'.$REX['TABLE_PREFIX'].'modultyp` , `'.$REX['TABLE_PREFIX'].'action` 
+          LEFT JOIN `'.$REX['TABLE_PREFIX'].'module_action` ON ( `'.$REX['TABLE_PREFIX'].'module_action`.`module_id` = `'.$REX['TABLE_PREFIX'].'modultyp`.`id` 
+            AND `'.$REX['TABLE_PREFIX'].'module_action`.`action_id` = `'.$REX['TABLE_PREFIX'].'action`.`id` ) 
+          WHERE `'.$REX['TABLE_PREFIX'].'modultyp`.`name` = "'.$modul_name.'"
+            AND `'.$REX['TABLE_PREFIX'].'action`.`name` = "'.$action_name.'"
+          LIMIT 1';
+  
+  $sql = new sql();
+  //$sql->debugsql = true;
+  $data = $sql->get_array($qry);
+
+  if (is_array($data) and $sql->getRows() == 1) {
+    foreach ($data as $row) {
+      // prüfe IDs auf vorhandensein
+      // sind diese IDs in dieser Kombination noch nicht in der Verknüpfungstabelle
+      // dann können sie dort eingetragen werden
+      if ($row['mod_action_m_id'] == 'false' and $row['mod_action_a_id'] == 'false') {
+        $qry = 'INSERT INTO `'.$REX['TABLE_PREFIX'].'module_action` ( `id` , `module_id` , `action_id` ) 
+                VALUES (NULL , "'.$row['m_id'].'", "'.$row['a_id'].'")';
+        $sql = new sql();
+        //$sql->debugsql = true;
+        $sql->setQuery($qry);
+      } else {
+        return 'installAction2Modul: Es exitiert bereits eine Zuweisung zwischen dem Modul "'.$modul_name.'" und der Aktion "'.$action_name.'".';
+      }
+    }
+  } else {
+    return 'installAction2Modul: Fehler in der Datenbankabfrage. Ist der Modulname "'.$modul_name.'" und der Aktionname "'.$action_name.'" richtig?'; 
+  }
+  return true;
+} // installAction2Modul()
 
 
 ?>
