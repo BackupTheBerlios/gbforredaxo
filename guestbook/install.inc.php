596493abafa84228589f5dd069918aa4642865f2
koala_s <koala_s>
2006-06-26 19:47:17 +0000

    Nur wenn die DB-Daten richtig installiert wurden, dürfen die Module auch installiert werden.

--- guestbook/install.inc.php
+++ guestbook/install.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: install.inc.php,v 1.3 2006/06/24 23:35:55 koala_s Exp $
+ * @version $Id: install.inc.php,v 1.4 2006/06/26 19:47:17 koala_s Exp $
  */
  
 $error = '';
@@ -14,9 +14,12 @@ if (!OOAddon :: isAvailable('addon_framework'))
   $error .= 'Required addon "addon_framework" is either not installed or not activated!';
 }
 
-if ($error == '')
-{
+if ($error == '') {
   $error .= rex_installAddon(dirname(__FILE__).'/install.sql');
+}
+
+// DB-Daten installiert? Dann hiermit weiter.
+if ($error == '') {
   include(dirname(__FILE__).'/install.php');
   // installAction2Modul(Name des Modules, Name der Action)
   $error .= installAction2Modul('Gästebuch - Eintragsliste', 'Gästebuch - Eintragsliste StatusPerDatei');
